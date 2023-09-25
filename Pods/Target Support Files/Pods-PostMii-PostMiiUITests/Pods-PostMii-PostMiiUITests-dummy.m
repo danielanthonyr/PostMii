3596651e573e766fa2bfb117e6e2491860e8f84f
Daniel-Anthony Romero <danielanthonyr@gmail.com>
2023-09-25 01:11:07 -0400

    .gitignore fixed

--- Pods/Target Support Files/Pods-PostMii-PostMiiUITests/Pods-PostMii-PostMiiUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_PostMii_PostMiiUITests : NSObject
-@end
-@implementation PodsDummy_Pods_PostMii_PostMiiUITests
-@end
--- Pods/Target Support Files/Pods-PostMii/Pods-PostMii-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_PostMii : NSObject
-@end
-@implementation PodsDummy_Pods_PostMii
-@end
